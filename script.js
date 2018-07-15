53a3f7919350c026ce17d44e2e55def9ffb6f84c
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-15 16:26:01 +0200

    Add format of obtained times method in script.js

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ class Stopwatch {
     print() {
         this.display.innerText = this.format(this.times);
     }
+
+    format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
 }
 
 const stopwatch = new Stopwatch(
