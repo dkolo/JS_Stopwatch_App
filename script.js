f258b9252cc8463de2773c69f111996df33bded2
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-15 16:48:25 +0200

    Add start method in Stopwatch class

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
 class Stopwatch {
     constructor(display) {
         this.running = false;
@@ -22,13 +30,12 @@ class Stopwatch {
         return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
     }
 
-    function pad0(value) {
-    let result = value.toString();
-    if (result.length < 2) {
-        result = '0' + result;
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
     }
-    return result;
-}
 }
 
 const stopwatch = new Stopwatch(
